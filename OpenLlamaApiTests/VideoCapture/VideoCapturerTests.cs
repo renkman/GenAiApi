c52007b4f3bb95aeec8302230b7c4d7eff232ce0
Jan Renken <jan.renken@posteo.de>
2025-10-26 20:59:18 +0100

    Enhance video capturing tests

--- OpenLlamaApiTests/VideoCapture/VideoCapturerTests.cs
+++ OpenLlamaApiTests/VideoCapture/VideoCapturerTests.cs
@@ -16,7 +16,7 @@ public class VideoCapturerTests : IClassFixture<WebApplicationFactory<Program>>
     }
     
     [Fact]
-    public void TryCapture_WithValidVideoStream_CapturesImageFromVideo()
+    public void TryCapture_WithValidVideoStream_CapturesImageFromVideoAndReturnsTrue()
     {
         var config = _factory.Services.GetRequiredService<VideoCaptureConfig>();
         var logger = _factory.Services.GetRequiredService<ILogger<VideoCapturer>>();
@@ -26,5 +26,19 @@ public class VideoCapturerTests : IClassFixture<WebApplicationFactory<Program>>
 
         Assert.True(result);
         Assert.NotEmpty(resultImage);
+    }
+    
+        
+    [Fact]
+    public void TryCapture_WithValidVideoStream_DoesNotCaptureAnImageFromVideoAndReturnsFalse()
+    {
+        var config = new VideoCaptureConfig { LiveCamPageUri = "Nothing_here!" };
+        var logger = _factory.Services.GetRequiredService<ILogger<VideoCapturer>>();
+        
+        var videoCapturer = new VideoCapturer(config, logger);
+        var result = videoCapturer.TryCapture(out var resultImage);
+
+        Assert.False(result);
+        Assert.Empty(resultImage);
     }
 }
